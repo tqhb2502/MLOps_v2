27857f650a6c0f872c030bf602f3059336e6ec6d
tqhb2502 <tqhb2502@gmail.com>
2024-12-29 00:03:58 +0700

    create config file

---
+++ src/config.py
@@ -0,0 +1,10 @@
+import os
+import yaml
+
+with open(os.path.join("config.yaml"), 'r') as file:
+    config = yaml.safe_load(file)
+
+DATA_CONFIG = config["data"]
+SYSTEM_CONFIG = config["system"]
+MODEL_CONFIG = config["model"]
+MSG_CONFIG = config["message"]
--- src/train.py
+++ src/train.py
@@ -6,33 +6,53 @@ from sklearn.linear_model import LinearRegression
 import pickle
 import os
 
-print(os.path.join('..', 'data', 'Salary_Data.csv'))
-dataset = pd.read_csv(os.path.join('data', 'Salary_Data.csv'))
+from config import DATA_CONFIG, SYSTEM_CONFIG, MODEL_CONFIG, MSG_CONFIG
+
+# Lấy config
+DATA_SOURCE = DATA_CONFIG['source']
+DATA_DIR = DATA_CONFIG['data-dir']
+TRAIN_RATIO = DATA_CONFIG['train-ratio']
+TEST_RATIO = DATA_CONFIG['test-ratio']
+
+RANDOM_STATE = SYSTEM_CONFIG['random-state']
+
+MODEL_DIR = MODEL_CONFIG['model-dir']
+MODEL_NAME = MODEL_CONFIG['model-name']
+SAVE_PROTOCOL = MODEL_CONFIG['save-protocol']
+
+MODEL_LOADED = MSG_CONFIG['model-loaded']
+NEW_MODEL = MSG_CONFIG['new-model']
+
+dataset = pd.read_csv(os.path.join(DATA_DIR, DATA_SOURCE))
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, 
+    test_size = TEST_RATIO/TRAIN_RATIO, 
+    random_state = RANDOM_STATE
+)
 
 # Đường dẫn tệp
-file_path = os.path.join("saved_model", "model.pkl")
+file_path = os.path.join(MODEL_DIR, MODEL_NAME)
 
 # Kiểm tra tệp tồn tại
 if os.path.exists(file_path):
     with open(file_path, "rb") as f:
         regressor = pickle.load(f)
-    print("Model loaded successfully!")
+    print(MODEL_LOADED)
 else:
     regressor = LinearRegression()
-    print("Create new model!")
+    print(NEW_MODEL)
 
 regressor.fit(X_train, y_train)
 
 # Tạo thư mục nếu chưa tồn tại
-os.makedirs("saved_model", exist_ok=True)
+os.makedirs(MODEL_DIR, exist_ok=True)
 
 # Save model
 with open(file_path, "wb") as f:
-    pickle.dump(regressor, f, protocol=5)
+    pickle.dump(regressor, f, protocol=SAVE_PROTOCOL)
 
 # Test & Ghi log
 y_pred = regressor.predict(X_test)
